f7e48d938293dc04a08b54f0e790c60c67806f0e
codermohit0718 <codermohit0718@gmail.com>
2025-12-06 12:18:25 +0530

    Add files via upload

---
+++ arithematic_operations.java
@@ -0,0 +1,10 @@
+public class arithematic_operations {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 30;
+        System.out.printf("the sum of the numbers:%d\n", (x+y));
+        System.out.printf("the substraction of the numbers:%d\n", (x-y));
+        System.out.printf("the product of the numbers:%d\n", (x*y));
+        System.out.printf("the division of the numbers:%d\n", (x/y));
+    }
+}
---
+++ variable_declaration.java
@@ -0,0 +1,8 @@
+public class variable_declaration {
+    public static void main(String[] args) {
+        int x;
+        x=500;
+        System.out.println(x);
+    }
+    
+}
