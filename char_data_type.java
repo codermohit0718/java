5708a94df129c4fb1050c50bd442b5544c6b7c6f
codermohit0718 <codermohit0718@gmail.com>
2025-12-08 18:13:55 +0530

    Add files via upload

---
+++ char_data_type.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class char_data_type {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = 'a';
+        System.out.printf("%C",ch);
+
+
+    }
+    
+}
